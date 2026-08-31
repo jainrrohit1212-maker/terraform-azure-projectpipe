c3b49c0bccf04b080cbdeadd1f87c99451022026
Rohit Jain <jainrrohit1212@gmail.com>
2026-09-01 00:58:26 +0530

    Add Terraform project files

---
+++ create-tfvars.ps1
@@ -0,0 +1,34 @@
+$csvFile = ".\resource.csv"
+$tfvarsFile = ".\terraform.tfvars"
+
+$csvFile = ".\resource.csv"
+$tfvarsFile = ".\terraform.tfvars"
+
+$lines = Get-Content $csvFile
+
+$output = "rgs = {`n"
+
+foreach ($line in $lines | Select-Object -Skip 1) {
+
+    if ([string]::IsNullOrWhiteSpace($line)) {
+        continue
+    }
+
+    $parts = $line -split '\s+'
+
+    $name = $parts[0]
+    $location = ($parts[1..($parts.Length - 1)] -join " ")
+
+    $output += "  $name = {`n"
+    $output += "    name     = `"$name`"`n"
+    $output += "    location = `"$location`"`n"
+    $output += "  }`n"
+}
+
+$output += "}`n"
+
+Set-Content -Path $tfvarsFile -Value $output
+
+Write-Host "terraform.tfvars created successfully!"
+Hover over a message to pin it
+keep
